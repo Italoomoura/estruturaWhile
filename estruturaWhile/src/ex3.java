e7f3e62462915fde2b3e598ebf08027ffc742c04
Italo Moura <97275782+Italoomoura@users.noreply.github.com>
2023-06-05 20:05:23 -0300

    Add files via upload

---
+++ estruturaWhile/src/ex3.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ex3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int alcool = 0;
+		int gasolina = 0;
+		int diesel = 0;
+		
+		int tipo = sc.nextInt();
+		
+		while (tipo != 4) {
+			switch(tipo) {
+				case 1:
+					alcool+=1;
+					break;
+				case 2:
+					gasolina+=1;
+					break;
+				case 3:
+					diesel+=1;
+					break;
+				default:
+					System.out.println("Fora da faixa de 1 a 4"); 
+					break;
+			}
+			
+			tipo = sc.nextInt();
+		}
+
+		System.out.println("MUITO OBRIGADO");
+		System.out.println("Alcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
+		
+		sc.close();
+	}
+
+}
